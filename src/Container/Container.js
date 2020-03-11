d261115d6bdb16fd62a3588e0dc0f9370005dee7
Thijs Busser <tbusser@gmail.com>
2020-03-11 16:10:05 +0100

    Fix code formatting
    The code was not formatted according to the .editorconfig rules. This
    has been corrected.

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -2,113 +2,113 @@ import { publish, subscribe } from '@ictoanen/pub-sub';
 
 /* ========================================================================== */
 
-function uniqueId(prefix = "") {
-  if(!uniqueId.num) {
-    uniqueId.num = 0;
-  }
+function uniqueId(prefix = '') {
+	if (!uniqueId.num) {
+		uniqueId.num = 0;
+	}
 
-  uniqueId.num += 1;
+	uniqueId.num += 1;
 
-  return prefix + uniqueId.num. toString();
+	return prefix + uniqueId.num.toString();
 }
 
 /* ========================================================================== */
 
 export class Container {
-  constructor({
-    state = {},
-    transforms = []
-  }) {
-    this.state = state;
-    this.transforms = transforms;
-    this.eventScope = { scope: uniqueId("container") };
+	constructor({ state = {}, transforms = [] }) {
+		this.state = state;
+		this.transforms = transforms;
+		this.eventScope = { scope: uniqueId('container') };
 
-    publish(this.events.ready, null, this.eventScope);
-  }
+		publish(this.events.ready, null, this.eventScope);
+	}
 
-  events = {
-    // The updated event is fired once all middleware is done,
-    // and the values inside the DataWrapper have updated correctly.
-    update: "UPDATE",
-    // The update event is fired once a value has updated.
-    beforeUpdate: "BEFORE_UPDATE",
-    // When the container is ready to be used.
-    ready: "READY"
-  };
+	events = {
+		// The updated event is fired once all middleware is done,
+		// and the values inside the DataWrapper have updated correctly.
+		update: 'UPDATE',
+		// The update event is fired once a value has updated.
+		beforeUpdate: 'BEFORE_UPDATE',
+		// When the container is ready to be used.
+		ready: 'READY'
+	};
 
-  /* ======================================================================== */
+	/* ====================================================================== */
 
-  onReady(callback) {
-    subscribe(
-      this.events.ready,
-      ({ detail }) => callback({
-        currentState: this.state,
-        updatedState: detail.updatedState
-      }),
-      this.eventScope
-    );
-  }
+	onReady(callback) {
+		subscribe(
+			this.events.ready,
+			({ detail }) =>
+				callback({
+					currentState: this.state,
+					updatedState: detail.updatedState
+				}),
+			this.eventScope
+		);
+	}
 
-  onUpdate(callback) {
-    subscribe(
-      this.events.update,
-      ({ detail }) => callback({
-        currentState: this.state,
-        updatedState: detail.updatedState
-      }),
-      this.eventScope
-    );
-  }
+	onUpdate(callback) {
+		subscribe(
+			this.events.update,
+			({ detail }) =>
+				callback({
+					currentState: this.state,
+					updatedState: detail.updatedState
+				}),
+			this.eventScope
+		);
+	}
 
-  onBeforeUpdate(callback) {
-    subscribe(
-      this.events.beforeUpdate,
-      ({ detail }) => callback({
-        currentState: this.state,
-        updatedState: detail.updatedState
-      }),
-      this.eventScope
-    );
-  }
+	onBeforeUpdate(callback) {
+		subscribe(
+			this.events.beforeUpdate,
+			({ detail }) =>
+				callback({
+					currentState: this.state,
+					updatedState: detail.updatedState
+				}),
+			this.eventScope
+		);
+	}
 
-  /* ======================================================================== */
+	/* ====================================================================== */
 
-  update = async(updatedState, options = {}) => {
-    publish(this.events.beforeUpdate, { updatedState }, this.eventScope);
+	update = async (updatedState, options = {}) => {
+		publish(this.events.beforeUpdate, { updatedState }, this.eventScope);
 
-    this.state = await this.transformState(
-      updatedState,
-      {
-        ...this.state,
-        ...updatedState
-      },
-      options
-    );
+		this.state = await this.transformState(
+			updatedState,
+			{
+				...this.state,
+				...updatedState
+			},
+			options
+		);
 
-    publish(this.events.update, { updatedState }, this.eventScope);
+		publish(this.events.update, { updatedState }, this.eventScope);
 
-    return this.state;
-  }
+		return this.state;
+	};
 
-  transformState(updatedState, state, options) {
-    return new Promise((resolve, reject) => {
-        try {
-          const transformedState = this.transforms.reduce(
-            async (currentState, transform) => ({
-                ...await currentState,
-                ...await transform({
-                  currentState,
-                  updatedState,
-                  options
-                })
-            }),
-            state
-          );
+	transformState(updatedState, state, options) {
+		return new Promise((resolve, reject) => {
+			try {
+				const transformedState = this.transforms.reduce(
+					async (currentState, transform) => ({
+						...(await currentState),
+						...(await transform({
+							currentState,
+							updatedState,
+							options
+						}))
+					}),
+					state
+				);
 
-          resolve(transformedState);
-        } catch(err) {
-          reject(err);
-        }
-    });
-  }
+				resolve(transformedState);
+			} catch (err) {
+				reject(err);
+			}
+		});
+	}
 }
