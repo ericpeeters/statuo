bd871a593610dd16edc74aa2977022c45cd326c2
Thijs Busser <tbusser@gmail.com>
2020-03-12 21:09:28 +0100

    Update build scripts
    The build script is updated so it will create an ES5 and an ES6 version
    of the package. The editorconfig had to be updated so the bash script
    is saved with LF line endings instead of the default CRLF.

--- build/index.sh
+++ build/index.sh
@@ -13,12 +13,17 @@ dir=${PACKAGE_OUTPUT_PATH:-"$root/dist"}
 echo "- Clean the destination folder"
 rm -rf "$dir"
 
-echo "- Transpile the TypeScript using tsc"
-tsc -p tsconfig.emit.json
+echo "- Transpile to ES5 version"
+env NODE_ENV='es5' babel src --source-root src --out-dir "$dir/" --ignore src/**/*.test.js --extensions .js --quiet
+echo "- Transpile to ES6 version"
+env NODE_ENV='es6' babel src --source-root src --out-dir "$dir/esm" --ignore src/**/*.test.js --extensions .js --quiet
 
 echo "- Transform and copy package.json"
-./scripts/transform.js
+./build/transform.js
+
+echo "- Remove test files from dist folder"
+find "$dir" -type f -name "*.test.js" -delete
 
 echo "- Copy README and License"
-cp LICENSE $dir
+cp LICENSE.md $dir
 cp README.md $dir
---
+++ build/transform.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+/* ========================================================================== *\
+	IMPORTS
+\* ========================================================================== */
+const { readFile, writeFile } = require('mz/fs');
+
+
+
+/* ========================================================================== *\
+	PRIVATE VARIABLES
+\* ========================================================================== */
+const
+	fileName = 'package.json',
+	path = require('path'),
+	rootPath = path.resolve(process.cwd()),
+	distPath = process.env.PACKAGE_OUTPUT_PATH || path.resolve(process.cwd(), 'dist');
+
+
+
+/* ========================================================================== *\
+	PRIVATE METHODS
+\* ========================================================================== */
+async function transform() {
+	const
+		rawData = await readFile(`${rootPath}/${fileName}`),
+		json = JSON.parse(rawData);
+
+	delete json.devDependencies;
+	delete json.scripts;
+
+	await writeFile(`${distPath}/${fileName}`, JSON.stringify(json, null, 4));
+}
+
+
+
+/* ========================================================================== *\
+	INITIALIZATION
+\* ========================================================================== */
+transform();
